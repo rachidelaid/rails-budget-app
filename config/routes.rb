573a68526d2148868a84d98e92c1734c128af323
racidelaid <racidelaid@gmail.com>
2022-05-23 19:30:50 +0100

    setup routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  
+  root "splash#index"
 
-  # Defines the root path route ("/")
-  # root "articles#index"
+  resources :categories, only: %i[index show new] do
+    resources :transactions, only: %i[index new]
+  end
 end

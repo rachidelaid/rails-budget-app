4c862d7709a2af9c9f07517aed8de0e86ec368c9
racidelaid <racidelaid@gmail.com>
2022-05-25 19:04:26 +0100

    transcation request test done

--- spec/requests/transactions_spec.rb
+++ spec/requests/transactions_spec.rb
@@ -1,4 +1,37 @@
 require 'rails_helper'
 
 RSpec.describe 'Transactions', type: :request do
+  let(:test_user) { create :user }
+  before { 
+    sign_in test_user
+    @category = Category.create(name: "apple",  logo: "apple.png", user_id: test_user.id)
+  }
+
+  describe 'GET /index' do
+    before(:each) do
+      get category_transactions_url(@category.id)
+    end
+
+    it 'renders a successful response' do
+      expect(response).to have_http_status(:ok)
+    end
+
+    it 'renders the correct template' do
+      expect(response.body).to include('Transactions')
+    end
+  end
+
+  describe 'GET /new' do
+    before(:each) do
+      get new_category_transaction_url(@category.id)
+    end
+
+    it 'renders a successful response' do
+      expect(response).to have_http_status(:ok)
+    end
+
+    it 'renders the correct template' do
+      expect(response.body).to include('New Transaction')
+    end
+  end
 end
